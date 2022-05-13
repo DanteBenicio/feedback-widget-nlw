83bf43ad4b2d451f4beb87b29243c647ebd1f12c
Dante Benicio <dantecosta79@gmail.com>
2022-05-12 21:40:43 -0300

    FeedbackTypeStep component

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -7,18 +7,23 @@ interface FeedbackTypeStepProps {
 
 export default function FeedbackTypeStep({ setFeedbackType }: FeedbackTypeStepProps) {
   return (
-    <div className="flex py-8 gap-2 w-full justify-center">
-      {Object.entries(feedbackTypes).map(([key, value]) => (
-        <button
-          type="button"
-          className="card"
-          key={key}
-          onClick={() => setFeedbackType(key as FeedbackType)}
-        >
-          <img src={value.image.src} alt={value.image.alt} className="w-10" />
-          <span>{value.title}</span>
-        </button>
-      ))}
-    </div>
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+      </header>
+      <div className="flex py-8 gap-2 w-full justify-center">
+        {Object.entries(feedbackTypes).map(([key, value]) => (
+          <button
+            type="button"
+            className="card"
+            key={key}
+            onClick={() => setFeedbackType(key as FeedbackType)}
+          >
+            <img src={value.image.src} alt={value.image.alt} className="w-10" />
+            <span>{value.title}</span>
+          </button>
+        ))}
+      </div>
+    </>
   );
 }
