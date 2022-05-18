45d4bb694ed3218b3a55df691625897b2fc224a5
Dante Benicio <dantecosta79@gmail.com>
2022-05-18 19:42:37 -0300

    changed port to 5000

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Server is Running');
 });
