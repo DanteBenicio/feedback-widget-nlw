3810eea73e392232cf002d70bddf9c0d3e493497
Dante Benicio <dantecosta79@gmail.com>
2022-05-17 21:59:11 -0300

    deleted

--- src/components/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CircleNotch } from 'phosphor-react';
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center p-1">
-      <CircleNotch weight="bold" className="w-5 h-5 animate-spin" />
-    </div>
-  );
-}
--- src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
